1124b507f037d6821c1e1781890e3389fa31f78c
Anubhav Dhawan <anubhav756@gmail.com>
2017-07-06 19:25:01 +0530

    client/server startup codes refactored

--- server/main.js
+++ server/main.js
@@ -1,8 +1,1 @@
-// collections
-import '/imports/api/images/images';
-
-// publications
-import '/imports/api/images/server/publications';
-
-// methods
-import '/imports/api/images/methods';
+import '/imports/startup/server';
