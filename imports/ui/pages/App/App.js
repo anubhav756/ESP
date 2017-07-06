44f0b9fb145719045d8a20b3c28fd829562514fa
Anubhav Dhawan <anubhav756@gmail.com>
2017-07-06 14:51:36 +0530

    App page refactored

---
+++ imports/ui/pages/App/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
+import { createContainer } from 'meteor/react-meteor-data';
+import Images from '/imports/api/images';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+
+    const url = this.urlInput.value.trim();
+
+    Images.insert({ url });
+    this.urlInput.value = '';
+  }
+  render() {
+    const { images } = this.props;
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" ref={(r) => { this.urlInput = r; }} placeholder="add image url" />
+        <div>{_.join(_.map(images, i => i.url), ', ')}</div>
+      </form>
+    );
+  }
+}
+App.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default createContainer(() => ({
+  images: Images.find({}, { sort: { url: 1 } }).fetch(),
+}), App);
