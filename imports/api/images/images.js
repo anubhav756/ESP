b7e35c82cc83ea840b17202dbef09f4a9fc2762d
Anubhav Dhawan <anubhav756@gmail.com>
2017-07-13 14:36:59 +0530

    schemas + validations added

--- imports/api/images/images.js
+++ imports/api/images/images.js
@@ -1,3 +1,13 @@
 import { Mongo } from 'meteor/mongo';
 
-export default new Mongo.Collection('images');
+const Images = new Mongo.Collection('images');
+
+Images.schema = new SimpleSchema({
+  _id: { type: SimpleSchema.RegEx.Id },
+  url: { type: SimpleSchema.RegEx.Url },
+  tags: { type: [String] },
+});
+
+Images.attachSchema(Images.schema);
+
+export default Images;
--- imports/api/rooms/rooms.js
+++ imports/api/rooms/rooms.js
@@ -1,4 +1,17 @@
 import { Mongo } from 'meteor/mongo';
+import Images from '/imports/api/images/images';
 
+const Rooms = new Mongo.Collection('rooms');
 export const MAX_ROOM_PLAYERS = 2;
-export default new Mongo.Collection('rooms');
+
+Rooms.schema = new SimpleSchema({
+  _id: { type: SimpleSchema.RegEx.Id },
+  players: { type: [SimpleSchema.RegEx.Id], max: MAX_ROOM_PLAYERS, min: 1 },
+  primary: { type: Images.schema, optional: true },
+  secondary: { type: [Images.schema], optional: true },
+  answers: { type: Object, blackbox: true, optional: true },
+});
+
+Rooms.attachSchema(Rooms.schema);
+
+export default Rooms;
