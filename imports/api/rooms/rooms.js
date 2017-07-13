14f2474390f5433e7ee4869aa7d0ef2f48c39b7c
Anubhav Dhawan <anubhav756@gmail.com>
2017-07-13 16:17:31 +0530

    room schema fixed

--- imports/api/rooms/rooms.js
+++ imports/api/rooms/rooms.js
@@ -6,7 +6,7 @@ export const MAX_ROOM_PLAYERS = 2;
 
 Rooms.schema = new SimpleSchema({
   _id: { type: SimpleSchema.RegEx.Id },
-  players: { type: [SimpleSchema.RegEx.Id], max: MAX_ROOM_PLAYERS, min: 1 },
+  players: { type: [SimpleSchema.RegEx.Id], maxCount: MAX_ROOM_PLAYERS, minCount: 1 },
   primary: { type: Images.schema, optional: true },
   secondary: { type: [Images.schema], optional: true },
   answers: { type: Object, blackbox: true, optional: true },
