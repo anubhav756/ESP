27a6aaeb8c8a9ce3391c6d2e8940d4d773f3788d
Anubhav Dhawan <anubhav756@gmail.com>
2017-07-14 20:26:14 +0530

    main app container snapshot test added

--- imports/ui/pages/Main/Main.tests.js
+++ imports/ui/pages/Main/Main.tests.js
@@ -2,21 +2,15 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { assert } from 'meteor/practicalmeteor:chai';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import ImagePreview from './ImagePreview';
+import Main from './Main';
 import snapshot from './snapshot.json';
 
 if (Meteor.isClient) {
-  describe('Image Preview component', function () {
+  describe('Main container', function () {
     it('renders correctly', () => {
-      const room = Factory.create('room');
       const tree = renderer.create(
         <MuiThemeProvider>
-          <ImagePreview
-            primary={room.primary}
-            secondary={room.secondary}
-            answers={room.answers}
-            handleNextClick={() => { }}
-          />
+          <Main />
         </MuiThemeProvider>,
       ).toJSON();
       assert.equal(
