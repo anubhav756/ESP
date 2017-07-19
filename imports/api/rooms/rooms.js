4e627673f908d244e4326350c882e8ec29a7857b
Anubhav Dhawan <anubhav756@gmail.com>
2017-07-19 16:04:01 +0530

    room schema updated to accept zero players

--- imports/api/rooms/rooms.js
+++ imports/api/rooms/rooms.js
@@ -5,7 +5,7 @@ export const MAX_ROOM_PLAYERS = 2;
 
 Rooms.schema = new SimpleSchema({
   _id: { type: SimpleSchema.RegEx.Id },
-  players: { type: [SimpleSchema.RegEx.Id], maxCount: MAX_ROOM_PLAYERS, minCount: 1 },
+  players: { type: [SimpleSchema.RegEx.Id], maxCount: MAX_ROOM_PLAYERS },
   primary: { type: Images.schema, optional: true },
   secondary: { type: [Images.schema], optional: true, minCount: 5, maxCount: 5 },
   answers: { type: Object, blackbox: true, optional: true },
